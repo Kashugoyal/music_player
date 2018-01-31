40331ab445e986b43c43d5a80fa268fef2040abe
Kashish <kashishgoyal2019@u.northwestern.edu>
2018-01-31 14:55:20 -0600

    Updated JS to correct timer

--- ipod.js
+++ ipod.js
@@ -1,12 +1,20 @@
 // Create your global variables below:
 var tracklist = ["Let It Happen", "Nangs", "The Moment", "Yes I'm Changing", "Eventually", "Gossip", "The Less I Know The Better", "Past Life", "Disciples", "'Cause I'm A Man"];
-var volLevels = ["vl0","vl1","vl2","vl3","vl4","vl5"];
-var k=3, time = 0, timer=0;
+var volLevels = [];
+// gloabal variables which we need to define
+var k=3, time = 0;
+// slider object definition
 var slider = document.getElementById("sliderval");
 
-
 function init() {
-	// Your code goes here #9f5cc4
+  for(var i = 0;i<6;i++){
+    volLevels[i]="vl"+ i;
+  }
+
+  // if we wanna see the output we can log the data
+  // console.log(volLevels);
+  // following loop initializes the volume level
+
   for (var i = 0;i<3; i++) {
     document.getElementById(volLevels[i]).style.backgroundColor="#9f5cc4";
   }
@@ -14,17 +22,17 @@ function init() {
 };
 
 function volUp() {
-	// Your code goes here
   k+=1;
+  // we add an if condition to account for boundary cases
   if(k>6){k=6;}
-  for (var i = 0;i<k; i++) {
+  for (var i = 0;i<k; i++){
     document.getElementById(volLevels[i]).style.backgroundColor="#9f5cc4";
   }
 }
 
 function volDown() {
-	// Your code goes here
   k-=1;
+  // if condition to account for boundary cases
   if(k<0){k=0;}
   for (var i = volLevels.length-1;i>=k; i--) {
     document.getElementById(volLevels[i]).style.backgroundColor="transparent";
@@ -32,8 +40,6 @@ function volDown() {
 }
 
 function switchPlay() {
-	// Your code goes here
-
   if(document.getElementById("playpause").innerHTML=="pause"){
     document.getElementById("playpause").innerHTML="play_arrow";
   }
@@ -43,26 +49,29 @@ function switchPlay() {
 }
 
 function nextSong() {
-	// Your code goes here
+  // local variables
   var num,i;
+  // we set the time to -1, this makes sure the timer starts from zero when the song changes(accounting for the delays), a condition is added below to make the timer still display 0:00
+  time = -1;
   for (i = 0; i < tracklist.length; i++) {
     if(tracklist[i]==document.getElementById("songname").innerHTML){
       num = (i+1)%(tracklist.length);
-      }}
-      document.getElementById("songname").innerHTML = tracklist[num];
-      time = 0;
-
+      }
+    }
+  document.getElementById("songname").innerHTML = tracklist[num];
 }
 
 function prevSong() {
 	// Your code goes here
   var num,i;
+  // same as above, we set the time to -1
+  time = -1;
   for (i = 0; i < tracklist.length; i++) {
     if(tracklist[i]==document.getElementById("songname").innerHTML){
       num = (tracklist.length+i-1)%(tracklist.length);
       }}
       document.getElementById("songname").innerHTML = tracklist[num];
-      time = 0;
+      
 }
 
 function secondsToMs(d) {
@@ -75,22 +84,27 @@ function secondsToMs(d) {
 }
 
 function set() {
-  // body...
+  // updates the value of the variable "time"
   time = event.target.value;
 }
 
 function timeup(){
-  if(time == 180){
+  if(time >= 180){
     nextSong();
   }
-  document.getElementById("time-elapsed").innerHTML = secondsToMs(time);
-  slider.value = time;
-
-  if(document.getElementById("playpause").innerHTML=="pause"){
+  // increment the time only if the play button is pressed
+    if(document.getElementById("playpause").innerHTML=="pause"){
     time++;
+  }
+  // we ad the if condition to ignore negative value of time, refer line 54
+  if(time>=0){
+    document.getElementById("time-elapsed").innerHTML = secondsToMs(time);
+    slider.value = time;
   }
 }
 
 init();
+// calling timup() every 1s
 setInterval(timeup,1000);
+// adding th elistener to slider change
 sliderval.addEventListener("input",set);
